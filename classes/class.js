89ecc5dbefee6cb6fef313149887a00367e92727
Andreas Christiansen <andycchhrr@gmail.com>
2023-05-15 16:16:39 +0200

    Sjette klasse!

--- classes/class.js
+++ classes/class.js
@@ -15,7 +15,18 @@ class Person {
 
 }
 
-chrisNg = new Person("Chiotoffer", "hater", "barn", "som", "leker")
+chrisNg = new Person("Chiotoffer", "hater", [1, 2 , 3, 4, 5, 6], "som", "leker")
 
-console.log(chrisNg.selvrespek())
+const displayAttr = document.createElement('p');
+displayAttr.textContent = "hiiiii"
+const txt = document.querySelector(".txt")
+txt.append(displayAttr)
+txt.after(chrisNg.torso.slice(3, 5))
 
+let OOO = [...chrisNg.torso, "tre", "fire"]
+
+console.log(OOO)
+
+OOO[2] = "JAAAAJ"
+
+console.log(OOO)
